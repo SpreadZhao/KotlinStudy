e2d5b40825f1054e59831e663760ba00f2f29cf5
SpreadZhao <www.spreadzhao@outlook.com>
2024-03-23 21:55:54 +0800

    bounded buffer

--- src/main/java/algo/Algo.java
+++ src/main/java/algo/Algo.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class Algo {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        while (num >= 0) {
-
-        }
+        
     }
 }

55dabc79e4cd982f9a98de48b043067f974ae281
SpreadZhao <www.spreadzhao@outlook.com>
2024-06-08 15:19:36 +0800

    unsafe HashMap

--- src/main/kotlin/concurrency/chm/UnsafeHashMap.kt
+++ src/main/kotlin/concurrency/chm/UnsafeHashMap.kt
@@ -1,5 +1,7 @@
 package concurrency.chm
 
+import java.util.*
+import kotlin.collections.HashMap
 import kotlin.concurrent.thread
 
 fun main() {
@@ -7,7 +9,7 @@ fun main() {
     val t = Thread({
         for (i in 0 until 100000) {
             thread(name = "ftf$i") {
-                map["$i"] = ""
+                map[UUID.randomUUID().toString()] = ""
             }
         }
     }, "ftf")

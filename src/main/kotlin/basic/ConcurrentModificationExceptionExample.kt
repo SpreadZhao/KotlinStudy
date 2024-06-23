e80322327e0cb5d44a9725b07101e6f08a32b9da
SpreadZhao <www.spreadzhao@outlook.com>
2024-06-23 16:24:01 +0800

    One question about reference

--- src/main/kotlin/basic/ConcurrentModificationExceptionExample.kt
+++ src/main/kotlin/basic/ConcurrentModificationExceptionExample.kt
@@ -1,5 +1,7 @@
 package basic
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 object ConcurrentModificationExceptionExample {
     fun test() {
         val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
@@ -8,8 +10,34 @@ object ConcurrentModificationExceptionExample {
         list.addAll(sublist)
         println("list: $list")
     }
+
+    fun test2() {
+        val list = ArrayList<String>()
+        list.add("item1")
+        list.add("item2")
+        val iterator = list.iterator()
+        list.add("item3")
+        while (iterator.hasNext()) {
+            val item = iterator.next()
+            println(item)
+        }
+        println("$list")
+    }
+
+    fun test3() {
+        val list = CopyOnWriteArrayList<String>()
+        list.add("item1")
+        list.add("item2")
+        val iterator = list.iterator()
+        list.add("item3")
+        while (iterator.hasNext()) {
+            val item = iterator.next()
+            println(item)
+        }
+        println("$list")
+    }
 }
 
 fun main() {
-    ConcurrentModificationExceptionExample.test()
+    ConcurrentModificationExceptionExample.test3()
 }
